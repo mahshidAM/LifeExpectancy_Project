eb02f61b41e1d4c1c5c372083f303265f15d4585
mahshidAM <70712870+mahshidAM@users.noreply.github.com>
2021-05-15 11:32:37 +0200

    Add data table, prepare map plotly

--- dashboard.py
+++ dashboard.py
@@ -1,31 +1,126 @@
-"""Interactive flask/plotly dashboard"""
-from flask import Flask, render_template,request , send_file, send_from_directory, make_response, Response, redirect,jsonify
-import os
-import io
+"""Interactive dash/plotly dashboard"""
 import pandas as pd
-import numpy as np
-import webbrowser
-from threading import Timer
-import logging
-#from dashboard_plots import *
-#from dashboardDB import *
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Output, Input
+from dashboard_plots import *
+import flask
+from flask import send_file, make_response, Response
+from io import *
 
 
-#db = DashboardDB()
+# external JavaScript files
+external_scripts = [
+    'https://www.google-analytics.com/analytics.js',
+    {'src': 'https://cdn.polyfill.io/v2/polyfill.min.js'},
+    {
+        'src': 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.10/lodash.core.js',
+        'integrity': 'sha256-Qqd/EfdABZUcAxjOkMi8eGEivtdTkh3b65xCZL4qAQA=',
+        'crossorigin': 'anonymous'
+    }
+]
 
-app = Flask(__name__) 
+# external CSS stylesheets
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    {
+        'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+        'rel': 'stylesheet',
+        'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+        'crossorigin': 'anonymous'
+    },
+    {
+        'href': 'https://use.fontawesome.com/releases/v5.8.1/css/all.css',
+        'rel': 'stylesheet',
+        'integrity': 'sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf',
+        'crossorigin': 'anonymous'
+    },
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+]
 
+graph = create_chart()
 
-@app.route("/") 
-def home(): 
-    return render_template('index.html', year='2000')
+df = pd.read_csv('data/API_SP_clean.csv', sep=',', encoding='utf8', engine='python')
+map_graph = create_map(df)
 
+app = dash.Dash(__name__,
+                external_scripts=external_scripts,
+                external_stylesheets=external_stylesheets)
 
+app.layout = html.Div(className='container main', children=[
+                               html.Div(className='row main-row',  # Define the row element
+                                       children=[
+                                          html.Div(className='col-6',
+                                                   children= [
+                                                     html.Article(className='card', 
+                                                                 children= [
+                                                                     html.Div(className='card-header',
+                                                                             children=[ 
+                                                                                 html.H2('Life Expectancy'),
+                                                                                 html.P(children=html.A(id='csv-button', n_clicks=0, className='fas fa-file-csv fa-2x download', href="/urlToDownload/") 
+                                                                                       )]                                                 
+                                                                                                
+                                                                             ),
+                                                                     html.Div(className='card-body text-secondary',
+                                                                              children=generate_table(df)
+                                                                             )
+                                                                         ])
+                                                   ]),  # Define the left element(table)
+                                          html.Div(className='col-6',
+                                                   children= [
+                                                     html.Article(className='card', 
+                                                                 children= [
+                                                                     html.Div(className='card-header',
+                                                                             children=html.H2('Map')
+                                                                             ),
+                                                                     html.Div(className='card-body text-secondary',
+                                                                             children=                                                                                                                                   dcc.Graph(
+                                                                                      id='map',
+                                                                                      figure=map_graph)
+                                                                             )]
+                                                                 )]
+                                                  )  # Define the right element(map)
+                                      ]),
+                                html.Div(className='row main-row',  # Define the row element
+                                       children=[
+                                          html.Div(className='col-12',
+                                                   children= [
+                                                     html.Article(className='card', 
+                                                                 children= [
+                                                                     html.Div(className='card-header',
+                                                                             children=html.H2('Chart')
+                                                                             ),
+                                                                     html.Div(className='card-body text-secondary',
+                                                                               children=                                                                                                                                   dcc.Graph(
+                                                                                      id='example-graph',
+                                                                                      figure=graph)
+                                                                             )]
+                                                                 )]
+                                                  ),  # Define the bottom element(chart)
+                                         
+                                      ])
+
+                            ])
+
+@app.server.route('/urlToDownload/') 
+def download_csv():
+    """
+    Return flask make_response (download file)
+    Call back from download button in html
+    """
+    response = make_response(df.to_csv())
+    response.headers["Content-Disposition"] = f"attachment; filename=data.csv"
+    response.headers["Content-Type"] = "text/csv"
+    return response
+            
+        
 '''def open_browser():
     webbrowser.open_new('http://127.0.0.1:2000/')'''
 
 def run_app():
-    app.run(debug = True)     
+    app.run_server()    
     """Timer(1, open_browser).start();
     app.logger.setLevel(logging.DEBUG)
     app.jinja_env.auto_reload = True
--- dashboard_plots.py
+++ dashboard_plots.py
@@ -1,244 +1,62 @@
-"""Visualizations(plotly) for flask dashboard."""
-import plotly
-import plotly.graph_objs as go
-from plotly.subplots import make_subplots
+"""Visualizations(plotly) for dash."""
+
+import dash_html_components as html
+import dash_table
+import plotly.express as px
 import plotly.graph_objects as go
-import json
-import math
 import pandas as pd
 
-def get_bubbleSize(data):
-    """
-    Returns bubble size for selected countries
-    Input - dataframe=data
-    """
-    hover_text = []
-    bubble_size = []
-
-    for index, row in data.iterrows():
-        hover_text.append(('Country: {country}<br>'+
-                           'Region: {region}<br>'+
-                          'Budget: {value}<br>'+
-                          'Year: {year}').format(country=row['country'],
-                                                 region=row['region'],
-                                                value=row['value'],
-                                                year=row['year']))
-        bubble_size.append(math.sqrt(row['value']))
-
-    data['text'] = hover_text
-    data['size'] = bubble_size
-    return data
-
-
-def create_bubbleChart(data):
-    """
-    Returns plotly bubbleChart in json format
-    Input - dataframe=data
-    """
-    
-    # Create figure
-    fig = go.Figure()
-    
-    data1=get_bubbleSize(data)   
-    
-    data1 = data1.sort_values(['region', 'country'])
-    region_name = list(data1.region.unique())
-    region_data = {region:data1.query("region == '%s'" %region)
-                              for region in region_name}
-
-    
-    for region_name, region in region_data.items():
-        fig.add_trace(go.Scatter(
-            x=region['CenterLongitude'], y=region['CenterLatitude'],
-            name=region_name, text=region['text'],
-            marker_size=region['size'],
-            )
-          )
-
-    # Tune marker appearance and layout
-    if len(data1['size']):
-        fig.update_traces(mode='markers', marker=dict(sizemode='area', 
-                                                  sizeref=(lambda x: 2.*max(x)/(100**2))(data1['size']), line_width=2), showlegend=True)
-
-    fig.update_layout(
-        title='Satellites budget in millions USD',
-        xaxis=dict(
-
-            gridcolor='rgb(243, 243, 243)',
-            type='linear',
-            gridwidth=2,
-        ),
-        yaxis=dict(
-        
-        gridcolor='rgb(243, 243, 243)',
-        gridwidth=2,
-    ),
-    #paper_bgcolor='rgb(243, 243, 243)',
-    #plot_bgcolor='rgb(243, 243, 243)',
-    paper_bgcolor='white',
-    plot_bgcolor='white',
-    yaxis_range=[-150.0,150.0]
+def generate_table(df):
+    return dash_table.DataTable(
+        id='table',
+        columns=[
+            {"name": i, "id": i, "selectable": True} for i in df.columns
+        ],
+        page_size=15,
+        style_cell={'padding': '5px',#'textAlign': 'right',
+                   'fontSize':12,'whiteSpace': 'normal',
+                   'height': 'auto'},
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+        style_data={
+            'whiteSpace': 'normal',
+            'height': 'auto',
+            'lineHeight': '15px'
+        },
+        data=df.to_dict('records'),
+        sort_action="native",
     )
 
-    
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return graphJSON
-    
+def create_map(df):
+    df1 = df.query("year == 2007")
+    print(df1)
+    '''data = df.query("year == 2007")
+    #fig = go.Figure(go.Scattergeo())
+    fig = px.choropleth(data, locations='code',
+                    color="value", # lifeExp is a column of gapminder
+                    hover_name="country", # column to add to hover information
+                    color_continuous_scale=px.colors.sequential.Plasma)'''
+    fig = px.choropleth(df1,  # Input Pandas DataFrame
+                    locations="code",  # DataFrame column with locations
+                    color="value",  # DataFrame column with color values
+                    hover_name="country") # DataFrame column hover info) # Set 
 
-def create_table(df):
-    """
-    Returns plotly data table in json format
-    Input - dataframe=df
-    """
-    table = go.Figure([go.Table(
-        columnorder = [1,2,3,4,5,6],
-        columnwidth = [140,80,140,140,100,100],
-        header=dict(
-            values=df.columns.str.replace('_',' '),
-            line_color='darkslategray',
-            align='left',
-            height=40
-        ),
-        cells=dict(
-            values=[df[k].tolist() for k in df.columns],
-            align = "left",
-            line_color='darkslategray',
-            fill_color='whitesmoke',
-            font=dict(color='midnightblue', size=12))
-        
-        )
-        
-    ])
- 
-    #table.update_layout(
-    #    autosize=True,
-    #    height=600,
-    #)
-    #tableJSON = json.dumps(table, cls=plotly.utils.PlotlyJSONEncoder)
-
-    #return tableJSON
-    return table
-
-def get_groupedYearData(country,df):
-    """
-    Returns dataframe filtered by country and grouped by year (for lineCharts)
-    Inputs - country=country, dataframe=data
-    """
-    grouped_df = df[df['country'] == country].groupby('year')['value'].sum().reset_index()
-    return grouped_df
-
-def create_lineChart(df):       
-    """
-    Returns plotly lineChart in json format
-    Input - dataframe=df
-    """
-    #fig = go.Figure()
-    fig = make_subplots(
-        rows=1, cols=2,
-        specs=[[{"type": "table"} , {"type": "scatter"}]])
-
-    #table = create_table(df)
-    fig.add_trace(
-        go.Table(
-            columnorder = [1,2,3,4,5,6,7,8],
-            columnwidth = [150,120,150,150,150,150,150,150],
-            header=dict(
-                values=df.columns.str.replace('_',' '),
-                line_color='darkslategray', fill_color='#fc5e61',
-                align='left', font=dict(color='white', size=10),
-                height=40
-            ),
-            cells=dict(
-                values=[df[k].tolist() for k in df.columns],
-
-                align = "left",
-                line_color='darkslategray',
-                fill_color='white',
-                font=dict(color='midnightblue', size=10)
-
-            )
-        ), 
-        row=1, col=1
-    )
-               
-    for name in list(df.country.unique()):
-        data = get_groupedYearData(name,df)
-        fig.add_trace(go.Scatter(x=data['year'], y=data['value'],
-                    mode='lines',
-                    name=name), row=1, col=2)
-       
-
-
-    fig.update_layout(
-        title='Evolution of Budget for each country',
-        xaxis=dict(
-
-            gridcolor='rgb(243, 243, 243)',
-            type='linear',
-            gridwidth=2,
-        ),
-        yaxis=dict(
-        
-        gridcolor='rgb(243, 243, 243)',
-        gridwidth=2,
-    ),
-     paper_bgcolor='white',
-     plot_bgcolor='white',
-     height=700
-    )
-    
-    # Add range slider
-    fig.update_layout(
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list([
-                    dict(count=1,
-                         label="1year",
-                         step="year",
-                         stepmode="backward"),
-                    dict(step="all")
-                ])
-            ),
-            rangeslider=dict(
-                visible=True
-            ),
-            type="date"
-        )
-    )
-
-
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return graphJSON
-
-def create_satellite_table(df):
-    """
-    Returns plotly data table in json format
-    Input - dataframe=df
-    """
-    table = go.Figure(data=[go.Table(
-      columnorder = [i for i,col in enumerate(list(df.columns))],
-      columnwidth = [120 for i,col in enumerate(list(df.columns))],
-      header=dict(
-        values=[f"<b>{k}<b>" for k in df.columns],
-
-        line_color='rgb(8, 81, 156)', fill_color='#fc5e61',
-
-        align='left', font=dict(color='white', size=10)
-      ),
-      cells=dict(
-        values=[df[k].tolist() for k in df.columns],
-        line_color='white', fill_color='white',
-        align='left', font=dict(color='#023161', size=10)
-      ))
-    ])
-    table.update_layout(
-        autosize=True,
-        height=1000,
-    )
-    
-    tableJSON = json.dumps(table, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return tableJSON
 
     
+    return fig
+
+def create_chart():
+    df = pd.DataFrame({
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    })
+
+    fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+    
+    return fig
+
+
+
